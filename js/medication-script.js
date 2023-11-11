2b28dc961fc0d2d7d113e8811de4ea89f80fe78b
Ishav Desai <ishavdesai@gmail.com>
2023-11-10 23:45:04 -0600

    (3/x) Search Bar Functionality Complete

--- js/medication-script.js
+++ js/medication-script.js
@@ -1,10 +1,21 @@
 let MedicationDict = {
+  'Amlodipine': 'Ca Channel Blocker',
   'Aspirin': 'PG Inhibitor',
+  'Ethosuximide': 'Absence Seizures',
+  'Fluoxetine': 'SSRI',
+  'Gabapentin': 'Neuropathic Pain',
+  'Lamotrigine': 'Maintenance for Bipolar Disorder',
   'Lisinopril': 'ACE Inhibitor',
-  'Metoprolol': 'Beta Blocker'
+  'Lithium': 'Bipolar Disorder',
+  'Metoprolol': 'Beta Blocker',
+  'Omeprazole': 'H+/K+ Inhibitor',
+  'Paroxetine': 'SSRI',
+  'Phenytoin': 'Broad Spectrum Seizures',
 };
 
 let MedicationMap = new Map(Object.entries(MedicationDict));
+
+let medications = Object.keys(MedicationDict);
 
 // Function to update the prescribed list in the UI
 function updatePrescribedList(selectedValues) {
@@ -35,7 +46,6 @@ function toggleMedicationSelection(event) {
 
 // Event listener for medication selection in the formulary list
 document.addEventListener("DOMContentLoaded", function () {
-  const medications = Object.keys(MedicationDict);
   const medicationList = document.getElementById('medicationList');
   medications.forEach((medication, index) => {
     const listItem = document.createElement('li');
@@ -43,6 +53,22 @@ document.addEventListener("DOMContentLoaded", function () {
     medicationList.appendChild(listItem);
 });
 });
+
+function searchFunctionality() {
+  document.getElementById('medicationSearchBar').addEventListener('input', function() {
+    var searchQuery = this.value.toLowerCase();
+    var textElements = document.querySelectorAll('span');
+    textElements.forEach(function(element) {
+      var textContent = element.textContent.toLowerCase();
+      // Show or hide text elements based on the search query
+      if (textContent.includes(searchQuery)) {
+          element.classList.remove('hidden');
+      } else {
+          element.classList.add('hidden');
+      }
+  });
+  });
+}
 
 function toggleSelection(index) {
   const medicationElement = document.querySelectorAll('.medication')[index];
